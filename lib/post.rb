90f9988fcd832f70a8b67a3ebfde322c83e10355
github username <github email address>
2017-12-14 21:32:06 -0500

    Done.

---
+++ lib/post.rb
@@ -0,0 +1,13 @@
+require_relative "author"
+
+class Post
+  attr_accessor :author, :title
+
+  def initialize (title)
+    @title = title
+  end
+
+  def author_name
+    @author ? @author.name : nil
+  end
+end
---
+++ lib/song.rb
@@ -0,0 +1,13 @@
+require_relative "artist"
+
+class Song
+  attr_accessor :artist, :name
+
+  def initialize(name)
+    @name = name
+  end
+
+  def artist_name
+    @artist ? @artist.name : nil
+  end
+end
